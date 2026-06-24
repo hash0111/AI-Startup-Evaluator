5f4489b36eab08226cee9c8bfd8ea04ad827e0df
hash0111 <swapnilawasthi007@gmail.com>
2026-06-24 22:45:20 +0530

    removing unused files

--- start-frontend.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-DIR="$(cd "$(dirname "$0")/frontend" && pwd)"
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-cd "$DIR"
-exec node ./node_modules/next/dist/bin/next dev --port 3000
